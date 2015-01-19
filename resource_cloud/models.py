c67fe817add2b3c1ee7f3851af0398154d68fe24
Harri Hämäläinen <hhamalai@iki.fi>
2015-01-19 08:59:13 +0200

    Changed to flask-bcrypt, fixes #3
    Changed to use flask-bcrypt instead of functions from werkzeug.

--- resource_cloud/models.py
+++ resource_cloud/models.py
@@ -1,4 +1,4 @@
-from werkzeug import generate_password_hash, check_password_hash
+from flask.ext.bcrypt import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 import uuid
 from wsgi import db, app
