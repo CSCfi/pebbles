00eeb9ea3a46addeb67044f911540f06ad83be97
Harri Hämäläinen <hhamalai@iki.fi>
2015-01-22 10:23:03 +0200

    Changed default message queue to Redis

--- resource_cloud/config.py
+++ resource_cloud/config.py
@@ -1,4 +1,8 @@
+# Flask configuration
 DEBUG = True
 SECRET_KEY = 'change_me'
 WTF_CSRF_ENABLED = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/change_me.db'
+
+# Messaging
+MESSAGE_QUEUE_URI = 'redis://localhost:6379/0'
--- resource_cloud/tasks.py
+++ resource_cloud/tasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
+import config
 
-app = Celery('tasks', broker='amqp://guest@localhost//')
+app = Celery('tasks', broker=config.MESSAGE_QUEUE_URI)
 
 
 @app.task(name="resource_cloud.tasks.run_provisioning")
