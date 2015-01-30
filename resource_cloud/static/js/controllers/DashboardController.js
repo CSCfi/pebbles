91bc3c2dc59b35a877a1ff3452ce028024450c93
tourunen <olli.tourunen@csc.fi>
2015-01-30 10:37:31 +0200

    * running service instances added to dashboard view * provisioning state changes with PATCH * asynchronous provisioning progress now tracked

--- resource_cloud/static/js/controllers/DashboardController.js
+++ resource_cloud/static/js/controllers/DashboardController.js
@@ -1,38 +1,71 @@
-app.controller('DashboardController', ['$scope', '$routeParams', 'AuthService', 'Restangular',
-                            function($scope, $routeParams, AuthService, Restangular) {
-    
-    Restangular.setDefaultHeaders({token: AuthService.getToken()});
+app.controller('DashboardController', ['$scope', '$routeParams', '$interval', 'AuthService', 'Restangular',
+    function ($scope, $routeParams, $interval, AuthService, Restangular) {
 
-    if (AuthService.isAdmin()) {
-        var users = Restangular.all('users');
-        users.getList().then(function(response) {
-            $scope.users = response;
-        });
+        Restangular.setDefaultHeaders({token: AuthService.getToken()});
 
-        $scope.new_user = '';
-        $scope.add_user = function(email) {
-            var user_parameters = { email: email };
-            if (email) {
-                users.post(user_parameters).then(function(response) {
-                    $scope.users.push(response);
+        if (AuthService.isAdmin()) {
+            var users = Restangular.all('users');
+            users.getList().then(function (response) {
+                $scope.users = response;
+            });
+
+            $scope.new_user = '';
+            $scope.add_user = function (email) {
+                var user_parameters = {email: email};
+                if (email) {
+                    users.post(user_parameters).then(function (response) {
+                        $scope.users.push(response);
+                    });
+                }
+            }
+
+            $scope.remove_user = function (user) {
+                user.remove().then(function () {
+                    var index = $scope.users.indexOf(user);
+                    if (index > -1) $scope.users.splice(index, 1);
                 });
             }
         }
 
-        $scope.remove_user = function(user) {
-            user.remove().then(function() {
-                var index = $scope.users.indexOf(user);
-                if (index > -1) $scope.users.splice(index, 1);
+        var resources = Restangular.all('resources');
+        resources.getList().then(function (response) {
+            $scope.services = response;
+        });
+
+        var provisionedResources = Restangular.all('provisioned_resources');
+        provisionedResources.getList().then(function (response) {
+            $scope.instances = response;
+        });
+
+        $scope.provision = function (resource) {
+            resource.post().then(function (response) {
+                    provisionedResources.getList().then(function (response) {
+                            $scope.instances = response;
+                        }
+                    )
+                }
+            )
+            ;
+        }
+
+        $scope.deprovision = function (provisionedResource) {
+
+//            provisionedResource.remove().then(function () {
+//                var index = $scope.instances.indexOf(provisionedResource);
+//                if (index > -1) $scope.instances.splice(index, 1);
+//            });
+            provisionedResource.patch({state:'deleting'}).then(function () {
+                var index = $scope.instances.indexOf(provisionedResource);
+                if (index > -1) $scope.instances[index].state='deleting';
             });
         }
-    }
 
-    var resources = Restangular.all('resources');
-    resources.getList().then(function(response) {
-          $scope.services = response;
-    });
-    
-    $scope.provision = function(resource) {
-        resource.post();
-    }
-}]);
+        if (users) {
+            $interval(function () {
+                var provisionedResources = Restangular.all('provisioned_resources');
+                provisionedResources.getList().then(function (response) {
+                    $scope.instances = response;
+                });
+            }, 10000);
+        }
+    }]);
