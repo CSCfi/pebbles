4f4c553ab7ae558fcf8a6bbe97cac6a708e00c8e
Harri Hämäläinen <hhamalai@iki.fi>
2015-02-02 17:49:28 +0200

    Added response to activation
    Included a response to user activation.

--- resource_cloud/static/js/controllers/ActivationController.js
+++ resource_cloud/static/js/controllers/ActivationController.js
@@ -1,8 +1,16 @@
 app.controller('ActivationController', ['$scope', '$routeParams', '$location', 'Restangular', function($scope, $routeParams, $location, Restangular) {
     var activations = Restangular.all('activations');
+    var activationSuccess = false;
+
     $scope.activate_user = function() {
         var token = $routeParams.token;
-        activations.post({token: token,
-                          password: $scope.password});
+        activations.post({token: token, password: $scope.password}).then(function(resp) {
+            activationSuccess = true;
+        });
     };
+
+    $scope.activationSuccess = function() {
+        return activationSuccess;
+    }
+
 }]);
