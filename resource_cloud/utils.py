4e2be9ff205e40951be44a44ce60461816f5ae2e
tourunen <olli.tourunen@csc.fi>
2015-02-05 23:19:20 +0200

    public key now exported with openssh -format so it is directly compatible with authorized_keys

--- resource_cloud/utils.py
+++ resource_cloud/utils.py
@@ -7,6 +7,6 @@ KEYPAIR_DEFAULT = {
 
 def generate_ssh_keypair(bits=KEYPAIR_DEFAULT['bits']):
     new_key = RSA.generate(bits)
-    public_key = new_key.publickey().exportKey("PEM")
-    private_key = new_key.exportKey("PEM")
+    public_key = new_key.publickey().exportKey(format="OpenSSH")
+    private_key = new_key.exportKey(format="PEM")
     return private_key, public_key
