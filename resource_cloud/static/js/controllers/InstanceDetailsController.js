724540f5844c2573e9cbedb63764ca6098c049c1
tourunen <olli.tourunen@csc.fi>
2015-03-03 15:38:32 +0200

    instance details enhancements
    * links to provisioning and deprovisioning logs are provided
    * page refreshed automatically
    (cherry picked from commit 81ea04c)

--- resource_cloud/static/js/controllers/InstanceDetailsController.js
+++ resource_cloud/static/js/controllers/InstanceDetailsController.js
@@ -5,16 +5,20 @@ app.controller('InstanceDetailsController', ['$q', '$http', '$routeParams', '$sc
 
         var instance_id = $routeParams.instance_id;
         var instance;
-        Restangular.one('provisioned_resources', instance_id).get().then(function (response) {
-            instance = response;
-            $scope.instance = response;
-        }).then(function () {
-            Restangular.one('resources', instance.resource_id).get().then(function (response) {
-                $scope.service = response;
-            });
-        });
 
-        $scope.refreshLogs = function (instance) {
+        $scope.refresh = function () {
+            Restangular.one('provisioned_resources', instance_id).get().then(function (response) {
+                instance = response;
+                $scope.instance = response;
+            }).then(function () {
+                Restangular.one('resources', instance.resource_id).get().then(function (response) {
+                    $scope.service = response;
+                });
+            });
+        };
+        $scope.refresh();
+
+        $scope.fetchLogs = function (instance) {
             if (!instance.logs.length) {
                 Restangular.one('provisioned_resources', instance.id).get().then(function (response) {
                     instance = response;
@@ -35,13 +39,20 @@ app.controller('InstanceDetailsController', ['$q', '$http', '$routeParams', '$sc
                 ).success(function (data, status, headers, config) {
                         var log_type = config['log_type'];
                         if (!$scope.logs)
-                            $scope.logs = [];
+                            $scope.logs = {};
                         $scope.logs[log_type] = data;
                     }
                 );
             }
         };
 
+        var statePollInterval = $interval(function () {
+            if (AuthService.isAuthenticated()) {
+                $scope.refresh();
+            } else {
+                $interval.cancel(statePollInterval);
+            }
+        }, 10000);
     }
 ])
 ;
