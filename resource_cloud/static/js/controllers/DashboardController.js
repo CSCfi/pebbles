cf9df508943c0495b8b66bee8d1e8b538aef3d64
Harri Hämäläinen <hhamalai@iki.fi>
2015-03-03 16:52:59 +0200

    Fixed interval handling with DashboardController

--- resource_cloud/static/js/controllers/DashboardController.js
+++ resource_cloud/static/js/controllers/DashboardController.js
@@ -21,23 +21,42 @@ app.controller('DashboardController', ['$q', '$scope', '$interval', 'AuthService
                     )
                 }
             );
-        }
+        };
 
         $scope.deprovision = function (provisionedResource) {
             provisionedResource.patch({state:'deleting'}).then(function () {
                 var index = $scope.instances.indexOf(provisionedResource);
                 if (index > -1) $scope.instances[index].state='deleting';
             });
+        };
+
+        var stop;
+        $scope.startPolling = function() {
+            if (angular.isDefined(stop)) {
+                return;
+            }
+            stop = $interval(function () {
+                if (AuthService.isAuthenticated()) {
+                    var provisionedResources = Restangular.all('provisioned_resources');
+                    provisionedResources.getList().then(function (response) {
+                        $scope.instances = response;
+                    });
+                } else {
+                    $interval.cancel(pollInterval);
+                }
+            }, 10000);
+        };
+
+        $scope.stopPolling = function() {
+            if (angular.isDefined(stop)) {
+                $interval.cancel(stop);
+                stop = undefined;
+            }
         }
 
-        var pollInterval = $interval(function () {
-            if (AuthService.isAuthenticated()) {
-                var provisionedResources = Restangular.all('provisioned_resources');
-                provisionedResources.getList().then(function (response) {
-                    $scope.instances = response;
-                });
-            } else {
-                $interval.cancel(pollInterval);
-            }
-        }, 10000);
+        $scope.$on('$destroy', function() {
+            $scope.stopPolling();
+        });
+
+        $scope.startPolling();
     }]);
