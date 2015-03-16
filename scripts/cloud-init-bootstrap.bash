58ba76add9d034c8830c16f39b026cd74bd0199f
tourunen <olli.tourunen@csc.fi>
2015-03-16 10:56:35 +0200

    bootstrap changes

--- scripts/cloud-init-bootstrap.bash
+++ scripts/cloud-init-bootstrap.bash
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-PB_BRANCH=wip/remote_deployment
-PB_REPO=https://github.com/CSC-IT-Center-for-Science/pouta-blueprints
+set -e
+set -x
 
 aptitude update
 aptitude install -y git build-essential python-dev python-setuptools
 easy_install pip
 pip install ansible
-git clone --branch $PB_BRANCH $PB_REPO /opt/pouta-blueprints
 
-cat > /root/pb_ansible_inventory << END_AI
-[docker_host]
-localhost ansible_connection=local
-END_AI
+cat > /tmp/pb_setup_run_as_cloud_user.bash << "END_RACU"
 
-ansible-playbook -i /root/pb_ansible_inventory /opt/pouta-blueprints/ansible/prepare_vm.yml
+#!/bin/bash
+PB_BRANCH=wip/remote_deployment
+PB_REPO=https://github.com/CSC-IT-Center-for-Science/pouta-blueprints
 
-su - cloud-user -c 'cd /opt/pouta-blueprints && scripts/deploy_local_docker_containers.bash'
+set -e
+set -x
 
+pb_temp=$(mktemp -d)
 
+git clone --branch $PB_BRANCH $PB_REPO $pb_temp/git
+
+(
+echo "[docker_host]"
+echo "localhost ansible_connection=local"
+) > $pb_temp/pb_ansible_inventory
+
+export PYTHONUNBUFFERED=1
+export ANSIBLE_SSH_ARGS="-o ControlMaster=no"
+ansible-playbook -i $pb_temp/pb_ansible_inventory $pb_temp/git/ansible/prepare_vm.yml
+
+cd $pb_temp/git && scripts/deploy_local_docker_containers.bash
+
+rm -rf $pb_temp
+
+END_RACU
+
+su - cloud-user -c "bash /tmp/pb_setup_run_as_cloud_user.bash"
