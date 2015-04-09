87698cf1632db38ed504d20d5b12567af063c241
Harri Hämäläinen <hhamalai@iki.fi>
2015-04-09 14:05:30 +0300

    Dynamically resolve config fields during runtime

--- pouta_blueprints/config.py
+++ pouta_blueprints/config.py
@@ -1,18 +1,20 @@
 import os
 import yaml
+import functools
 
 CONFIG_FILE = '/etc/pouta_blueprints/config.yaml'
-CONFIG = {}
-if os.path.isfile(CONFIG_FILE):
-    CONFIG = yaml.load(open(CONFIG_FILE).read())
 
 
-def resolve_configuration_value(key, default=None):
+def resolve_configuration_value(key, default=None, *args, **kwargs):
     pb_key = 'PB_' + key
     value = os.getenv(pb_key)
     if value:
         return value
-    elif key in CONFIG:
+
+    CONFIG = {}
+    if os.path.isfile(CONFIG_FILE):
+        CONFIG = yaml.load(open(CONFIG_FILE).read())
+    if key in CONFIG:
         return CONFIG[key]
     elif default:
         return default
@@ -20,17 +22,22 @@ def resolve_configuration_value(key, default=None):
         raise RuntimeError('configuration value for %s missing' % key)
 
 
+def fields_to_properties(cls):
+    for k, default in vars(cls).items():
+        if not k.startswith('_'):
+            resolvef = functools.partial(resolve_configuration_value, k, default)
+            setattr(cls, k, property(resolvef))
+    return cls
+
+
+@fields_to_properties
 class BaseConfig(object):
     DEBUG = True
-    SECRET_KEY = resolve_configuration_value('SECRET_KEY', default='change_me')
+    SECRET_KEY = "change_me"
     WTF_CSRF_ENABLED = False
     SSL_VERIFY = False
-    SQLALCHEMY_DATABASE_URI = resolve_configuration_value(
-        'SQLALCHEMY_DATABASE_URI',
-        default='sqlite:////tmp/change_me.db')
-    M2M_CREDENTIAL_STORE = resolve_configuration_value(
-        'M2M_CREDENTIAL_STORE',
-        default='/var/run/pouta_blueprints_m2m')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/change_me.db'
+    M2M_CREDENTIAL_STORE = '/var/run/pouta_blueprints_m2m'
     MESSAGE_QUEUE_URI = 'redis://www:6379/0'
     INSTANCE_DATA_DIR = '/var/spool/pb_instances'
     INTERNAL_API_BASE_URL = 'https://www/api/v1'
