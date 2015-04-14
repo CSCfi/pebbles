baa63114b815e2911d82a57c54d4546918c0b09f
tourunen <olli.tourunen@csc.fi>
2015-04-14 16:21:50 +0300

    added human readable lifetime display support
    fixes #134
    
    * new element <lifetime seconds="N" /> now supported
    * LIfetimeDirective.js added for rendering the lifetime
    * dashboard, configure and instance details views updated to make use of the new element

---
+++ pouta_blueprints/static/js/directives/LifetimeDirective.js
@@ -0,0 +1,44 @@
+// create a custom directive for displaying instance lifetime in human readable format
+app.directive('lifetime', function () {
+        function link(scope, element, attributes) {
+
+            // Here we have to use a attribute change listener. The first time 'link' is fired
+            // we might not have the data loaded to scope yet.
+            attributes.$observe('seconds', function (newValue) {
+                    var nSecs = parseInt(newValue, 10);
+
+                    var days = Math.floor(nSecs / (3600 * 24));
+                    var secsLeft = nSecs - days * 3600 * 24;
+
+                    var hours = Math.floor(secsLeft / 3600);
+                    secsLeft -= hours * 3600;
+
+                    var minutes = Math.floor(secsLeft / 60);
+                    secsLeft -= minutes * 60;
+
+                    var seconds = secsLeft;
+
+                    var timeStr;
+                    if (days == 0 && hours == 0 && minutes == 0) {
+                        timeStr = seconds + ' s';
+                    }
+                    else if (days == 0 && hours == 0 && minutes < 5) {
+                        timeStr = minutes + 'm ' + seconds + ' s';
+                    }
+                    else {
+                        timeStr = hours + 'h ' + minutes + 'm';
+                        if (days) {
+                            timeStr = days + 'd ' + timeStr;
+                        }
+                    }
+                    element.text(timeStr);
+                }
+            );
+        }
+
+        return {
+            restrict: 'E',
+            link: link
+        };
+    }
+);
