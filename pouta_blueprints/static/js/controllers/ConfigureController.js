ca8da7fac61b3081a374a9dbac42f72e881255ac
Harri Hämäläinen <hhamalai@iki.fi>
2015-04-28 14:58:03 +0300

    Frontend changes to support runtime variables

--- pouta_blueprints/static/js/controllers/ConfigureController.js
+++ pouta_blueprints/static/js/controllers/ConfigureController.js
@@ -11,6 +11,12 @@ app.controller('ConfigureController', ['$q', '$scope', '$http', '$interval', 'Au
 
         var blueprints = Restangular.all('blueprints');
 
+        $scope.variables = [
+            {key: 'SMTP_HOST', value: 'smtp.example.com'},
+            {key: 'SMTP_PORT', value: 25}
+        ];
+
+
         blueprints.getList({show_deactivated: true}).then(function (response) {
             $scope.blueprints = response;
         });
@@ -74,5 +80,9 @@ app.controller('ConfigureController', ['$q', '$scope', '$http', '$interval', 'Au
         $scope.deactivate = function (blueprint) {
             blueprint.is_enabled = undefined;
             blueprint.put();
+        };
+
+        $scope.updateVariable = function(variable) {
+            console.log(variable);
         };
     }]);
