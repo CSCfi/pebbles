9fc22e12f8b7c9d647cffef4a51298a9880be5c9
tourunen <olli.tourunen@csc.fi>
2015-09-03 14:40:23 +0300

    only enable plugins that are considered stable by default

--- pouta_blueprints/config.py
+++ pouta_blueprints/config.py
@@ -70,7 +70,7 @@ class BaseConfig(object):
     DEFAULT_QUOTA = 1.0
     ENABLE_SHIBBOLETH_LOGIN = False
     INSTALLATION_NAME = 'Pouta Blueprints'
-    PLUGIN_WHITELIST = ''
+    PLUGIN_WHITELIST = 'OpenStackDriver PvcCmdLineDriver'
 
     DD_SHUTDOWN_MODE = True
     DD_HOST_IMAGE = 'CentOS-7.0'
