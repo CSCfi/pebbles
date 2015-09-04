86b3c98d565555d4d98fc86ea6d33a1bf2d58d85
Harri Hämäläinen <hhamalai@iki.fi>
2015-09-04 12:43:20 +0300

    ...

--- protractor/conf.js
+++ protractor/conf.js
@@ -7,3 +7,12 @@ exports.config = {
         browserName: 'chrome'
     }]
 };
+if (process.env.TRAVIS) {
+    exports.config.sauceUser = process.env.SAUCE_USERNAME;
+    exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    exports.config.capabilities = {
+        'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER
+    };
+}
