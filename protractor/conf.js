e599c25815346509e9415c72d171baf7a912ece2
Harri Hämäläinen <hhamalai@iki.fi>
2015-09-04 14:33:01 +0300

    ...

--- protractor/conf.js
+++ protractor/conf.js
@@ -1,17 +1,15 @@
-exports.config = {
-    specs: ['blueprints-spec.js'],
-    multiCapabilities: [{
-        browserName: 'firefox'
-    }, {
-        browserName: 'chrome'
-    }]
+config = {
+    specs: ['blueprints-spec.js']
 };
+
 if (process.env.TRAVIS) {
-    exports.config.sauceUser = process.env.SAUCE_USERNAME;
-    exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-    exports.config.capabilities = {
+    config.sauceUser = process.env.SAUCE_USERNAME;
+    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    config.capabilities = {
         'browserName': 'chrome',
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER
     };
 }
+
+exports.config = config
