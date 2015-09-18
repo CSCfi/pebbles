eed2bccad9f6dc0f437534ee6eb1ef5f18d9ba7c
tourunen <olli.tourunen@csc.fi>
2015-09-18 10:41:48 +0300

    changed logic to loop to keep supervisord happy

--- ansible/roles/proxy/templates/usr/local/sbin/wait_and_reload_nginx.sh
+++ ansible/roles/proxy/templates/usr/local/sbin/wait_and_reload_nginx.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-inotifywait -e modify,close_write,moved_to,create $1
+if [ "$#" -ne 1 ]; then
+        echo "filename to watch is missing"
+        exit 1
+fi
 
-/etc/init.d/nginx reload
+while [ -e $1 ]; do
+        now=$(date --rfc-3339=seconds); now=${now%\n}
+        echo "$now waiting for changes in $1"
+
+        inotifywait -q -t 60 -e modify,close_write,moved_to,create $1
+
+        /etc/init.d/nginx reload
+done
+
+now=$(date --rfc-3339=seconds); now=${now%\n}
+echo "$now exiting"
