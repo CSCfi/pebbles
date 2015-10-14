74c99cfdeca503e34f9208742f63e70200d9c436
tourunen <olli.tourunen@csc.fi>
2015-10-14 12:35:41 +0300

    periodic_update sampling rewritten
    - all instances checked
    - instances needing action are appended to lists
    - 10 instances are picked from deprovisioning list
    - 10 instances are picked from other update list
    - update messages are sent

--- pouta_blueprints/tasks.py
+++ pouta_blueprints/tasks.py
@@ -124,28 +124,37 @@ def periodic_update():
     pbclient = PBClient(token, flask_config['INTERNAL_API_BASE_URL'], ssl_verify=False)
     instances = pbclient.get_instances()
 
-    # do not spam more than 10 updates at one interval
-    if len(instances) > 10:
-        instances = random.sample(instances, 10)
-
+    deprovision_list = []
+    update_list = []
     for instance in instances:
         logger.debug('checking instance for actions %s' % instance['name'])
         deprovision_required = False
         if instance.get('state') in [Instance.STATE_RUNNING]:
             if not instance.get('lifetime_left') and instance.get('maximum_lifetime'):
-                logger.info('deprovisioning triggered for %s (reason: maximum lifetime exceeded)' % instance.get('id'))
                 deprovision_required = True
 
             if deprovision_required:
-                pbclient.do_instance_patch(instance['id'], {'to_be_deleted': True})
-                run_update.apply_async(
-                    args=[token, instance.get('id')],
-                    queue='system_tasks',
-                )
+                deprovision_list.append(instance)
+
         elif instance.get('state') not in [Instance.STATE_FAILED]:
-                run_update.apply_async(
-                    args=[token, instance.get('id')]
-                )
+            update_list.append(instance)
+
+    if len(deprovision_list) > 10:
+        deprovision_list = random.sample(deprovision_list, 10)
+    for instance in deprovision_list:
+        logger.info('deprovisioning triggered for %s (reason: maximum lifetime exceeded)' % instance.get('id'))
+        pbclient.do_instance_patch(instance['id'], {'to_be_deleted': True})
+        run_update.apply_async(
+            args=[token, instance.get('id')],
+            queue='system_tasks',
+        )
+
+    if len(update_list) > 10:
+        update_list = random.sample(update_list, 10)
+    for instance in update_list:
+        run_update.apply_async(
+            args=[token, instance.get('id')]
+        )
 
 
 @app.task(name="pouta_blueprints.tasks.send_mails")
