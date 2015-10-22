2758bc4b649f26bab44be70239bd860215ddffe0
tourunen <olli.tourunen@csc.fi>
2015-10-22 07:05:12 +0300

    switched to absolute imports

--- pouta_blueprints/tasks/__init__.py
+++ pouta_blueprints/tasks/__init__.py
@@ -1,17 +1,17 @@
-import celery_app
+import pouta_blueprints.tasks.celery_app
 
-import provisioning_tasks
-import proxy_tasks
-import misc_tasks
+import pouta_blueprints.tasks.provisioning_tasks
+import pouta_blueprints.tasks.proxy_tasks
+import pouta_blueprints.tasks.misc_tasks
 
-run_update = provisioning_tasks.run_update
+run_update = pouta_blueprints.tasks.provisioning_tasks.run_update
 
-update_user_connectivity = provisioning_tasks.update_user_connectivity
+update_user_connectivity = pouta_blueprints.tasks.provisioning_tasks.update_user_connectivity
 
-proxy_add_route = proxy_tasks.proxy_add_route
+proxy_add_route = pouta_blueprints.tasks.proxy_tasks.proxy_add_route
 
-proxy_remove_route = proxy_tasks.proxy_remove_route
+proxy_remove_route = pouta_blueprints.tasks.proxy_tasks.proxy_remove_route
 
-send_mails = misc_tasks.send_mails
+send_mails = pouta_blueprints.tasks.misc_tasks.send_mails
 
-periodic_update = misc_tasks.periodic_update
+periodic_update = pouta_blueprints.tasks.misc_tasks.periodic_update
