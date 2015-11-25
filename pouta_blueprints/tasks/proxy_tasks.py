21342a148eeadfe20608fbd7ffef1f2053750d36
tourunen <olli.tourunen@csc.fi>
2015-11-25 10:23:01 +0200

    frontend and proxy work
    - including proxy config directly
    - nginx_reload now watches for proxy.conf in the new location
    - docker driver uses the new EXTERNAL_HTTPS_PORT variable + notebook -prefix
    - proxy_tasks now generates just a list of locations, not the whole config
    - config.py now has the new EXTERNAL_HTTPS_PORT variable

--- pouta_blueprints/tasks/proxy_tasks.py
+++ pouta_blueprints/tasks/proxy_tasks.py
@@ -1,13 +1,10 @@
 import glob
 from string import Template
-
 import os
-
 from pouta_blueprints.tasks.celery_app import logger, get_config
-
 from pouta_blueprints.tasks.celery_app import celery_app
 
-RUNTIME_PATH = '/webapps/pouta_blueprints/run'
+RUNTIME_PATH = '/webapps/pouta_blueprints/run/proxy_conf.d'
 
 
 @celery_app.task(name="pouta_blueprints.tasks.proxy_add_route")
@@ -18,10 +15,10 @@ def proxy_add_route(route_key, target, no_rewrite_rules=False):
     # see https://support.rstudio.com/hc/en-us/articles/200552326-Running-with-a-Proxy
     template = Template(
         """
-        location /${route_key}/ {
-          ${no_rw}rewrite ^/${route_key}/(.*)$$ /$$1 break;
+        location /notebooks/${route_key}/ {
+          ${no_rw}rewrite ^/notebooks/${route_key}/(.*)$$ /$$1 break;
           proxy_pass ${target};
-          ${no_rw}proxy_redirect ${target} $$scheme://$$host:${external_https_port}/${route_key};
+          ${no_rw}proxy_redirect ${target} $$scheme://$$host:${external_https_port}/notebooks/${route_key};
           proxy_set_header Upgrade $$http_upgrade;
           proxy_set_header Connection "upgrade";
         }
@@ -61,15 +58,7 @@ def proxy_remove_route(route_key):
 
 
 def refresh_nginx_config():
-    config = [
-        'server {',
-        '   listen %s;' % get_config()['EXTERNAL_HTTPS_PORT'],
-        '   ssl on;',
-        '   ssl_certificate /etc/nginx/ssl/server.crt;',
-        '   ssl_certificate_key /etc/nginx/ssl/server.key;',
-        '   client_max_body_size 20M;',
-    ]
-
+    config = []
     nroutes = 0
     pattern = '%s/route_key-*' % RUNTIME_PATH
     for proxy_route in glob.glob(pattern):
@@ -77,14 +66,7 @@ def refresh_nginx_config():
         with open(proxy_route, 'r') as f:
             config.extend(x.rstrip() for x in f.readlines())
 
-    config.append('}')
-    config.append('')
-
     logger.debug('refresh_nginx_config(): added %d routes' % nroutes)
-
-    # path = '/etc/nginx/sites-enabled/proxy'
-    # path = '/tmp/proxy.conf'
     path = '%s/proxy.conf' % RUNTIME_PATH
-
     with open(path, 'w') as f:
         f.write('\n'.join(config))
