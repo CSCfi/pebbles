c7153ecce4630b0053df3487ff7a2f4692f789aa
Jyrsa <jyry.suvilehto@iki.fi>
2017-04-24 11:44:45 +0300

    doc for simple availability monitoring (#600)

--- pebbles/views/variables.py
+++ pebbles/views/variables.py
@@ -80,6 +80,13 @@ class VariableView(restful.Resource):
 
 
 class PublicVariableList(restful.Resource):
+    """The list of variables that are needed for the frontend to construct a branded page.
+    Installation name, logo url etc.
+
+    Additionally this is the only API call that does not require authentication and therefore it is a good place to
+    monitor that the back-end (and database connection) are healthy.
+    """
+
     @marshal_with(variable_fields)
     def get(self):
         try:
