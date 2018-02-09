a8fe1237dd24487b508deaee9b2dbafa2ec82af7
Ilavarasi1 <sheethal.jan@gmail.com>
2018-02-09 16:57:33 +0200

    Increase the length of acceptable public ssh keys. Fixes #686. (#688)

--- migrations/env.py
+++ migrations/env.py
@@ -74,6 +74,7 @@ def run_migrations_online():
     connection = engine.connect()
     context.configure(connection=connection,
                       target_metadata=target_metadata,
+                      compare_type=True,
                       process_revision_directives=process_revision_directives,
                         render_as_batch=config.get_main_option('sqlalchemy.url').startswith('sqlite:///'),
                       **current_app.extensions['migrate'].configure_args)
