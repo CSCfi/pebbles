d4cf6ad25d40160a3d18db71855c53c7070a57e0
Ilavarasi1 <sheethal.jan@gmail.com>
2018-03-16 15:54:10 +0000

    Display username for rstudio when launching.Closes #705.

--- pebbles/static/js/controllers/DashboardController.js
+++ pebbles/static/js/controllers/DashboardController.js
@@ -228,6 +228,10 @@ app.controller('DashboardController', ['$q', '$scope', '$routeParams', '$interva
                         instance: function(){
                            return instance;
                         },
+
+                        blueprint: function(){
+                           return _.filter($scope.blueprints, { 'id': instance.blueprint_id });
+                        },
                     }   
                 }).result.then(function(markedInstances) {
                        window.open(instance.instance_data['endpoints'][0].access, '_blank');
@@ -305,8 +309,9 @@ app.controller('DashboardController', ['$q', '$scope', '$routeParams', '$interva
         $scope.startPolling();
     }]);
 
-app.controller('ModalShowPasswordController', function($scope, $modalInstance, instance) {
-    $scope.instance = instance
+app.controller('ModalShowPasswordController', function($scope, $modalInstance, instance, blueprint) {
+    $scope.instance = instance;
+    $scope.blueprint = blueprint;
     $scope.copyAndClose = function() {
         try {
             var passwordField = document.querySelector('#password');
