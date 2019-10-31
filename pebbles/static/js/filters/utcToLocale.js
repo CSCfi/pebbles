b926f39d066b4e780d82e6dd6de4fa83c1d6a8ba
Ilavarasi1 <sheethal.jan@gmail.com>
2019-10-31 10:27:06 +0000

    Make expiration date for blueprints visible in the UI. Also display time in UTC for the entire system. Closes #844

--- pebbles/static/js/filters/utcToLocale.js
+++ pebbles/static/js/filters/utcToLocale.js
@@ -1,12 +1,7 @@
 app.filter('utcToLocale', function ($filter) {
-    /* This will add 'Z' to denote that api returned time in UTC.
-     * so local time will be displayed in UI. */
     return function (input, format) {
         if (input) {
-            if (input.indexOf('Z') === -1 && input.indexOf('+') === -1) {
-                input += 'Z';
-            }
-            return $filter('date')(input, format);
+            return $filter('date')(new Date(input), format);
             }
         };
 });
