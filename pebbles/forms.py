a463032f25a65a91480eb098e12e23511cf18838
Olli Tourunen <tourunen@csc.fi>
2026-05-09 00:07:30 +0300

    Refactor forms.py
    - remove redundant WTForms-Alchemy
    - inherit the form classes directly from FlaskForm.
    - drop unused forms: ApplicationTemplateImportForm,
      ApplicationImportForm, VariableForm
    - drop MAX_VARIABLE_KEY_LENGTH and MAX_VARIABLE_VALUE_LENGTH

--- pebbles/forms.py
+++ pebbles/forms.py
@@ -4,25 +4,14 @@ cause gray hair.
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, IntegerField
 from wtforms.validators import DataRequired, Email, Length, AnyOf
-from wtforms_alchemy import model_form_factory
 
 from pebbles.models import (
     MAX_EMAIL_LENGTH, MAX_NAME_LENGTH,
-    MAX_VARIABLE_KEY_LENGTH, MAX_VARIABLE_VALUE_LENGTH,
     MAX_MESSAGE_SUBJECT_LENGTH
 )
-from pebbles.models import db
-
-BaseModelForm = model_form_factory(FlaskForm)
 
 
-class ModelForm(BaseModelForm):
-    @classmethod
-    def get_session(cls):
-        return db.session
-
-
-class UserForm(ModelForm):
+class UserForm(FlaskForm):
     ext_id = StringField('ext_id', validators=[DataRequired(), Email(), Length(max=MAX_EMAIL_LENGTH)])
     email_id = StringField('email_id', validators=[Email(), Length(max=MAX_EMAIL_LENGTH)])
     password = StringField('password', default=None)
@@ -34,7 +23,7 @@ WS_TYPE_LONG_RUNNING = 'long-running-course'
 VALID_WS_TYPES = [WS_TYPE_FIXED_TIME, WS_TYPE_LONG_RUNNING]
 
 
-class WorkspaceForm(ModelForm):
+class WorkspaceForm(FlaskForm):
     name = StringField('name')
     description = StringField('description')
     expiry_ts = IntegerField()
@@ -45,12 +34,12 @@ class WorkspaceForm(ModelForm):
     contact = StringField('contact')
 
 
-class MessageForm(ModelForm):
+class MessageForm(FlaskForm):
     subject = StringField('subject', validators=[DataRequired(), Length(max=MAX_MESSAGE_SUBJECT_LENGTH)])
     message = StringField('message', validators=[DataRequired()])
 
 
-class ServiceAnnouncementForm(ModelForm):
+class ServiceAnnouncementForm(FlaskForm):
     subject = StringField('subject', validators=[DataRequired()])
     content = StringField('content', validators=[DataRequired()])
     level = IntegerField('level', default=1)
@@ -59,14 +48,14 @@ class ServiceAnnouncementForm(ModelForm):
     is_public = BooleanField('is_public', default=False)
 
 
-class ApplicationTemplateForm(ModelForm):
+class ApplicationTemplateForm(FlaskForm):
     name = StringField('name', validators=[DataRequired(), Length(max=MAX_NAME_LENGTH)])
     base_config = StringField('base_config', validators=[DataRequired()])
     attribute_limits = StringField('attribute_limits')
     is_enabled = BooleanField('is_enabled', default=False)
 
 
-class ApplicationForm(ModelForm):
+class ApplicationForm(FlaskForm):
     name = StringField('name', validators=[DataRequired(), Length(max=MAX_NAME_LENGTH)])
     description = StringField('description')
     template_id = StringField('template_id', validators=[DataRequired()])
@@ -77,41 +66,20 @@ class ApplicationForm(ModelForm):
     workspace_id = StringField('workspace_id', validators=[DataRequired()])
 
 
-class ApplicationTemplateImportForm(ModelForm):
-    name = StringField('name', validators=[DataRequired()])
-    config = StringField('config', validators=[DataRequired()])
-    cluster_name = StringField('cluster_name', validators=[DataRequired()])
-    attribute_limits = StringField('attribute_limits')
-
-
-class ApplicationImportForm(ModelForm):
-    name = StringField('name', validators=[DataRequired()])
-    config = StringField('config', validators=[DataRequired()])
-    template_name = StringField('template_name', validators=[DataRequired()])
-    workspace_name = StringField('workspace_name', validators=[DataRequired()])
-
-
-class ApplicationSessionForm(ModelForm):
+class ApplicationSessionForm(FlaskForm):
     application_id = StringField('application_id', validators=[DataRequired()])
 
 
-class SessionCreateForm(ModelForm):
+class SessionCreateForm(FlaskForm):
     ext_id = StringField('ext_id', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
 
 
-class VariableForm(ModelForm):
-    key = StringField(
-        'key', validators=[DataRequired(), Length(max=MAX_VARIABLE_KEY_LENGTH)])
-    value = StringField(
-        'value', validators=[Length(max=MAX_VARIABLE_VALUE_LENGTH)])
-
-
-class LockForm(ModelForm):
+class LockForm(FlaskForm):
     owner = StringField('owner')
 
 
-class CustomImageForm(ModelForm):
+class CustomImageForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     workspace_id = StringField('workspace_id', validators=[DataRequired()])
     definition = StringField('image_content', validators=[DataRequired()])
